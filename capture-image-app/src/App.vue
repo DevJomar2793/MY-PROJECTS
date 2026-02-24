3cd4c281248b3aa308c00dc37d753e5de2738c63
Jomar Cerrado <jomarcerrado2793@gmail.com>
2026-02-24 14:12:56 +0800

    try testing

--- capture-image-app/src/App.vue
+++ capture-image-app/src/App.vue
@@ -3,7 +3,6 @@ import Footer from "../src/components/footer.vue";
 </script>
 <template>
   <div class="d-flex flex-column min-vh-100">
-    <router-link to="/dashboard">Dashboard</router-link>
     <router-view />
     <Footer />
   </div>
