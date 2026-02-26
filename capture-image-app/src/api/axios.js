9868265e41086df5bcb269309d4ad814207775a0
Jomar Cerrado <jomarcerrado2793@gmail.com>
2026-02-26 15:04:44 +0800

    docs: Add comment explaining the axios timeout duration.

--- capture-image-app/src/api/axios.js
+++ capture-image-app/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const api = axios.create({
     baseURL: "https://image-capture-app-90x2.onrender.com",
-    timeout: 60000,
+    timeout: 60000, //60s
     withCredentials: false,
 })
 
