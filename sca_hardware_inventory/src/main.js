9305a710ac5b281f0f751712c13521bb22c10fa4
Jomar Cerrado <jomarcerrado2793@gmail.com>
2026-03-13 16:40:23 +0800

    chore: Install project dependencies.

---
+++ sca_hardware_inventory/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import '../static/style.css'
+import App from './App.vue'
+import router from './router/router'
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Bootstrap JS (for modal, dropdown, tooltip, etc.)
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+createApp(App).use(router).mount('#app')
---
+++ sca_hardware_inventory/src/router/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from '../view/index.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Index',
+        component: Index
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
+    
