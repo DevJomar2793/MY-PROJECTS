ee4cace06d4d26cb4cfc5850b044bbcebee90301
Jomar Cerrado <jomarcerrado2793@gmail.com>
2026-04-15 15:43:19 +0800

    Updated

--- sca-sitemap-vue/backend/database.py
+++ sca-sitemap-vue/backend/database.py
@@ -12,7 +12,7 @@ try:
 except Exception:
     pass
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://db_projects_fqbk_user:fSUhzFrdqOckOUFylHRFSBsGv6E6bdru@dpg-d7fiarnaqgkc739r2fbg-a.singapore-postgres.render.com/db_projects_fqbk")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://db_projects_fqbk_user:fSUhzFrdqOckOUFylHRFSBsGv6E6bdru@dpg-d7fiarnaqgkc739r2fbg-a/db_projects_fqbk")
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is missing in environment variables")
